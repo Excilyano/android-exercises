cd822ee0220425f348a7761d08e766ce5e5dff5d
Excilyano <agahnim13@gmail.com>
2017-11-09 15:16:27 +0100

    Exercice 6

--- app/src/main/java/fr/android/androidexercises/Book.java
+++ app/src/main/java/fr/android/androidexercises/Book.java
@@ -1,8 +1,9 @@
 package fr.android.androidexercises;
 
-// TODO use parcel
+import android.os.Parcel;
+import android.os.Parcelable;
 
-class Book {
+class Book implements Parcelable{
 
     private String name;
     private String author;
@@ -11,6 +12,34 @@ class Book {
         this.name = name;
         this.author = author;
     }
+
+    protected Book(Parcel in) {
+        name = in.readString();
+        author = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(author);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
 
     public String getName() {
         return name;
